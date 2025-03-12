ecffbec395bc79c5a1d3c70bd53e21d8529f7f68
Coolcord <coolcord24@tutanota.com>
2025-03-12 10:53:41 -0700

    Volume now has a N/A status

--- volume.lua
+++ volume.lua
@@ -9,19 +9,20 @@ function update_volume(widget)
    local status = fd:read("*all")
    fd:close()
  
-   -- local volume = tonumber(string.match(status, "(%d?%d?%d)%%")) / 100
    local volume = string.match(status, "(%d?%d?%d)%%")
-   volume = string.format("% 3d", volume)
- 
-   status = string.match(status, "%[(o[^%]]*)%]")
+   if volume then
+     volume = string.format("% 3d", volume)
+     status = string.match(status, "%[(o[^%]]*)%]")
 
-   if string.find(status, "on", 1, true) then
-       -- For the volume numbers
-       volume = "V:" .. volume .. "%"
+     if string.find(status, "on", 1, true) then
+         -- For the volume numbers
+         volume = "V:" .. volume .. "%"
+     else
+         -- For the mute button
+         volume = "V:" .. volume .. "M"
+     end
    else
-       -- For the mute button
-       volume = "V:" .. volume .. "M"
-       
+     volume = "V: N/A"
    end
    widget:set_markup(volume)
 end
